9c42d2c9d1dfeae51ab8cf2648b7a2d2b3938005
synox <synox.github@dubgo.com>
2021-03-18 20:08:50 +0100

    set mount

--- icloud_photos_downloader.sh
+++ icloud_photos_downloader.sh
@@ -2,9 +2,11 @@
 # Make sure it's not already running
 pgrep -f icloudpd && echo "icloudpd is already running." && exit
 
+MOUNT=mount-google-drive/Photos
+
 # Make sure the mount is active
-if [ ! -d mount-google-drive/Photos/ ]; then
-  echo mount-google-drive/Photos is not mounted; 
+if [ ! -d $MOUNT/ ]; then
+  echo $MOUNT is not mounted;
   exit 1
 fi
 
@@ -16,7 +18,7 @@ if [[ "$1" == "cron" ]]; then
   email_seetings="--smtp-username $gmail_username --smtp-password $gmail_password --notification-email $recipient"
 fi
 
-icloudpd --directory ./mount-google-drive/Photos \
+icloudpd --directory ./$MOUNT \
 --username $email \
 --password $password \
 $email_seetings \
